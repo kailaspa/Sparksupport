14c3a2f11020eb679b4fd6bd039a008e722808e2
Kailas <42216683+kailaspa@users.noreply.github.com>
2024-02-05 20:34:39 +0530

    Update main class to remove entity scan

--- src/main/java/com/sparksupport/product/SparksupportApplication.java
+++ src/main/java/com/sparksupport/product/SparksupportApplication.java
@@ -2,14 +2,12 @@ package com.sparksupport.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  * Main application class for Sparksupport. Configures and launches the Spring
  * Boot application.
  */
 @SpringBootApplication
-@EntityScan(basePackages = "com.sparksupport.product.entity")
 public class SparksupportApplication {
 
 	/**
